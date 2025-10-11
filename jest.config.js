058e21e07930b33a20011d019cb66038763b0513
Francisco Quintero <frankjrdev@gmail.com>
2025-10-11 22:43:23 +0200

    feat: configuración inicial del proyecto con TypeScript y ESLint

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    roots: ['<rootDir>/src'],
+    testMatch: ['**/__tests__/**/*.test.ts'],
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest'
+    },
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1'
+    },
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    collectCoverageFrom: [
+        'src/**/*.{ts,tsx}',
+        '!src/**/*.d.ts',
+        '!src/**/*.test.ts',
+        '!src/server.ts',
+        '!src/app.ts'
+    ]
+};
